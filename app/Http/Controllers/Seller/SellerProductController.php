292dea2e808428b735fe14a942ddce9d4e177350
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-04-12 17:10:12 +0530

    Implemented Image Upload

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -7,6 +7,7 @@ use App\Product;
 use App\Seller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
@@ -41,7 +42,7 @@ class SellerProductController extends ApiController
 
         $data = $request->all();
         $data['status'] = Product::UNAVAILABLE_PRODUCT;
-        $data['image'] = 'img-1.jpg';
+        $data['image'] = $request->image->store('');
         $data['seller_id'] = $seller->id;
 
         $product = Product::create($data);
@@ -81,6 +82,11 @@ class SellerProductController extends ApiController
             }
         }
 
+        if($request->hasFile('image')) {
+            Storage::delete($product->image);
+            $product->image = $request->image;
+        }
+
         if ($product->isClean()) {
             return $this->errorResponse('You need to specify a different value to update', 422);
         }
@@ -99,6 +105,7 @@ class SellerProductController extends ApiController
     {
         $this->checkSeller($seller, $product);
         $product->delete();
+        Storage::delete($product->image);
         return $this->showOne($product);
     }
 
